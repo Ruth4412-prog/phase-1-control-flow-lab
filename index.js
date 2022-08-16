72dd94ab31a8abba8c3e5e7318e396d6aa0563fb
Ruth4412-prog <kipchauijepkaa@gmail.com>
2022-08-16 11:01:06 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(sample){
+if( sample<=400 ){
+  return `This one is on me!`;
+
+}
+else if(sample >2000 && sample <2500){
+  return `I will gladly take your thirty bucks.`;
+}
+else if(sample >2500){
+  return `No can do.`;
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const getCity = city ==="NYC" ? "Ok, sounds good." : "No go.";
+  return getCity;
+  
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(msg){
+switch (msg){
+  case (msg= "generous"):
+    return "Thank you so much.";
+    break;
+    case (msg="not as generous"):
+      return"Thank you.";
+      break;
+      default:
+        return "Bye.";
+}
 }
